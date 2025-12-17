99289791cd1737cbb6dc662f1f1f55216ecb70c1
FirdavsF15 <firdavsf2005@gmail.com>
2025-12-17 16:12:36 -0500

    update

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -1,45 +1,48 @@
-import { useEffect, useRef, useState } from 'react'
+import { useContext, useEffect, useRef, useState } from "react";
+import { TaskContext } from "./TaskContext";
 
-function TaskForm({ addTask }) {
-  // Creates a state to store our description input value
-  const [description, setDescription] = useState('')
+function TaskForm() {
+   // Creates a state to store our description input value
+   const [description, setDescription] = useState("");
 
-  /*
-   * The useRef hook allows us to reference a value not needed for rendering
-   * It's useful for manipulating the DOM
-   * Initialized to null because this is declared before rendering
-   * */
-  const inputRef = useRef(null)
+   const { addTask } = useContext(TaskContext);
 
-  useEffect(() => {
-    // Focuses the description form input
-    inputRef?.current?.focus()
-  }, []) // no dependency = on mount, run this logic
+   /*
+    * The useRef hook allows us to reference a value not needed for rendering
+    * It's useful for manipulating the DOM
+    * Initialized to null because this is declared before rendering
+    * */
+   const inputRef = useRef(null);
 
-  const handleSubmit = (e) => {
-    e.preventDefault()
+   useEffect(() => {
+      // Focuses the description form input
+      inputRef?.current?.focus();
+   }, []); // no dependency = on mount, run this logic
 
-    // addTask is passed as prop from App.jsx to use here
-    addTask(description)
-    // reset our form input value
-    setDescription('')
-  }
+   const handleSubmit = (e) => {
+      e.preventDefault();
 
-  return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor='description'>Description</label>
-      <input
-        type='text'
-        id='description'
-        name='description'
-        value={description}
-        ref={inputRef}
-        onChange={(e) => {
-          setDescription(e.target.value)
-        }}
-      />
-      <button type='submit'>Add Task</button>
-    </form>
-  )
+      // addTask is passed as prop from App.jsx to use here
+      addTask(description);
+      // reset our form input value
+      setDescription("");
+   };
+
+   return (
+      <form onSubmit={handleSubmit}>
+         <label htmlFor="description">Description: </label>
+         <input
+            type="text"
+            id="description"
+            name="description"
+            value={description}
+            ref={inputRef}
+            onChange={(e) => {
+               setDescription(e.target.value);
+            }}
+         />
+         <button type="submit">Add Task</button>
+      </form>
+   );
 }
-export default TaskForm
+export default TaskForm;
